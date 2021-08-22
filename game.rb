abf8bc1c48d4960016c49de7ab16c3769b454a68
Mindy Johnson <mindy.l.brock@gmail.com>
2021-08-22 18:40:07 -0500

    Added method to generate a random secret code.

--- game.rb
+++ game.rb
@@ -1,7 +1,15 @@
 class Game
-  attr_reader :board
-  
+  attr_reader :board, :secret_code
+
   def initialize
     @board = Board.new
+  end
+
+  def play
+    game_setup
+  end
+
+  def game_setup
+    @secret_code = Array.new(4) {rand(1..6).to_s}
   end
 end
--- main.rb
+++ main.rb
@@ -2,4 +2,6 @@ require_relative 'game.rb'
 require_relative 'board.rb'
 require_relative 'players.rb'
 
-Game.new.board.display_board(5326)
+my_game = Game.new
+my_game.play
+p my_game.secret_code
