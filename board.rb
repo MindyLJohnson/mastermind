c13d92395d9c6641971ceeffbf5ed581bce1ed78
Mindy Johnson <mindy.l.brock@gmail.com>
2021-08-22 21:29:43 -0500

    Added feature to print out the clues.

--- board.rb
+++ board.rb
@@ -1,14 +1,23 @@
 class Board
   CODE_COLORS = {
-    '1' => "\e[41m\e[1m 1 \e[0m",
-    '2' => "\e[42m\e[1m 2 \e[0m",
-    '3' => "\e[43m\e[1m 3 \e[0m",
-    '4' => "\e[44m\e[1m 4 \e[0m",
-    '5' => "\e[45m\e[1m 5 \e[0m",
-    '6' => "\e[46m\e[1m 6 \e[0m"
+    '1' => "\e[1;41m 1 \e[0m",
+    '2' => "\e[1;42m 2 \e[0m",
+    '3' => "\e[1;43m 3 \e[0m",
+    '4' => "\e[1;44m 4 \e[0m",
+    '5' => "\e[1;45m 5 \e[0m",
+    '6' => "\e[1;46m 6 \e[0m"
   }
 
-  def display_board(guess)
-    guess.to_s.split('').each {|code| print "#{CODE_COLORS[code]} "}
+  CLUE_SYMBOLS = {
+    '*' => "\e[1;31m\u25CF \e[0m",
+    '?' => "\e[1;37m\u25CF \e[0m",
+    '_' => "\e[1;37m\u25CB \e[0m"
+  }
+
+  def display_board(guess, clues)
+    guess.each {|code| print "#{CODE_COLORS[code]} "}
+    print "\e[1m Clues: \e[0m"
+    clues.each {|code| print "#{CLUE_SYMBOLS[code]}"}
+    print "\n"
   end
 end
--- game.rb
+++ game.rb
@@ -3,6 +3,8 @@ require_relative 'user_interface.rb'
 class Game
   include UserInterface
 
+  attr_reader :board, :guess, :secret_code, :clues
+
   def initialize
     @board = Board.new
   end
@@ -11,11 +13,30 @@ class Game
     game_setup
 
     puts player_guess_prompt
-    guess = gets.chomp
-    board.display_board(guess)
+    @guess = gets.chomp.to_s.split('')
+
+    get_clues
+
+    board.display_board(guess, clues)
   end
 
   def game_setup
     @secret_code = Array.new(4) {rand(1..6).to_s}
+  end
+
+  def get_clues
+    @clues = []
+
+    guess.each_index do |index|
+      if guess[index] == secret_code[index]
+        clues << '*'
+      elsif guess.include?(secret_code[index])
+        clues << '?'
+      else
+        clues << '_'
+      end
+    end
+
+    clues.sort!
   end
 end
