0122410a0fc57309e17cd5573dd843c91a5a701f
Mindy Johnson <mindy.l.brock@gmail.com>
2021-08-23 20:55:33 -0500

    Added code to validate the player's guess.

--- game.rb
+++ game.rb
@@ -11,26 +11,26 @@ class Game
 
   def play
     game_setup
-
     until code_cracked?
       puts player_guess_prompt
       @guess = gets.chomp.to_s.split('')
-
+      until valid_guess?
+        puts invalid_guess_prompt
+        @guess = gets.chomp.to_s.split('')
+      end
       get_clues
       board.display_board(guess, clues)
     end
   end
 
   def game_setup
-    p @secret_code = Array.new(4) {rand(1..6).to_s}
-    
+    p @secret_code = Array.new(4) {rand(1..6).to_s}    
     puts player_mode_prompt
     @player_mode = gets.chomp
   end
 
   def get_clues
     @clues = []
-
     guess.each_index do |index|
       if guess[index] == secret_code[index]
         clues << '*'
@@ -40,8 +40,11 @@ class Game
         clues << '_'
       end
     end
-
     clues.sort!
+  end
+
+  def valid_guess?
+    guess.length == 4 && guess.all? {|code| code.to_i.between?(1, 6)}
   end
 
   def code_cracked?
--- user_interface.rb
+++ user_interface.rb
@@ -1,9 +1,14 @@
 module UserInterface
-  def player_guess_prompt
-    "Make an attempt at cracking the code!"
-  end
-
   def player_mode_prompt
     "Would you rather be the code MAKER or the code BREAKER?"
   end
+
+  def player_guess_prompt
+    "Make an attempt at cracking the code! (4-digits between 1 and 6)"
+  end
+
+  def invalid_guess_prompt
+    "Invalid guess. Please input a 4-digit number between 1 and 6."
+  end
+
 end
