9e16187d0112348bbd65dddb6a251242e7d31e4b
Mindy Johnson <mindy.l.brock@gmail.com>
2021-09-06 16:19:46 -0500

    Optimized computer solving strategy.

--- game.rb
+++ game.rb
@@ -22,6 +22,7 @@ class Game
   def initialize
     @board = Board.new
     @remaining_guesses = POSSIBLE_GUESSES.collect { |code| code.to_s.split('') }
+    @possible_guesses = remaining_guesses.clone
     @first_guess = true
   end
 
@@ -65,14 +66,46 @@ class Game
   def computer_crack_code
     @guess = first_guess ? %w[1 1 2 2] : new_guess
     @first_guess = false
+    possible_guesses.delete(guess)
   end
 
   def new_guess
-    remaining_guesses.keep_if do |remaining_guess|
-      board.create_clues(remaining_guess, guess).eql?(clues)
-    end
-    p board.create_clues(%w[1 2 3 4], guess).eql?(clues)
+    reduce_guesses
+    next_guess
     remaining_guesses.min
+  end
+
+  def reduce_guesses
+    remaining_guesses.keep_if do |remaining_guess|
+      equal_clues?(remaining_guess, guess, clues)
+    end
+  end
+
+  def next_guess
+    score_guesses
+    select_guess
+  end
+
+  def score_guesses
+    @guess_scores = []
+    possible_guesses.each do |guess|
+      guess_score = 0
+      POSSIBLE_CLUES.each do |clue|
+        remaining_guesses.each do |remaining_guess|
+          guess_score += 1 if equal_clues?(remaining_guess, guess, clue)
+        end
+      end
+      guess_scores << guess_score
+    end
+  end
+
+  def select_guess
+    max_index = guess_scores.index(guess_scores.max)
+    possible_guesses[max_index]
+  end
+
+  def equal_clues?(guess, code, clues)
+    board.create_clues(guess, code).eql?(clues)
   end
 
   def code_cracked?
